ed40304829063c9e04297607363440a43dd7f062
griftt <1361255708@qq.com>
2019-06-10 10:03:33 +0800

    add config center

--- src/main/java/com/griftt/configserver/ConfigServerApplication.java
+++ src/main/java/com/griftt/configserver/ConfigServerApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableDiscoveryClient
 public class ConfigServerApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
